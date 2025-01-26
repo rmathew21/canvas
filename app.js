9690eaffa2ea3929d8095e78037ba70aed8357f0
rob <robmathew21@gmail.com>
2025-01-26 00:08:50 -0600

    changed width of pointer/cursor

--- app.js
+++ app.js
@@ -7,20 +7,39 @@ ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 50;
+// ctx.globalCompositeOperation = 'multiply';
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true
 
 function draw(e) {
    if (!isDrawing) return; 
     // e.preventDefault(); 
     console.log('Mouse move event:', e);
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    
     ctx.beginPath(); 
     ctx.moveTo(lastX, lastY); // start from
     ctx.lineTo(e.offsetX, e.offsetY); // go to
     ctx.stroke();
     [lastX, lastY] = [e.offsetX, e.offsetY];
+
+    hue++;
+    if(hue >= 360) {
+        hue = 0;
+    }
+    if(ctx.lineWidth >= 100 || ctx.lineWidth<=1) {
+        direction = !direction;
+    }
+    if (direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
+    
 }
 
 canvas.addEventListener('pointermove', draw);
